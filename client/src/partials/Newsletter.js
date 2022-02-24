7e80c65f6169dc86f22028e4d2837f0e296e0058
wissem <46422516+wissemayadi@users.noreply.github.com>
2022-02-24 23:02:18 +0100

    Update Newsletter.js

--- client/src/partials/Newsletter.js
+++ client/src/partials/Newsletter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Newsletter() {
-  return (
+  return ( 
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pb-12 md:pb-20">
